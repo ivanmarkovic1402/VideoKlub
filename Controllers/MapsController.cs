27fd1b7839bac2c5a19f3d4bc1cd19df32877539
ivan_markovic <ivan.markovic1402@gmail.com>
2020-10-13 15:50:09 +0200

    Map
    Google Map added

---
+++ Controllers/MapsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using VideoKlub.Models;
+
+namespace VideoKlub.Controllers
+{
+    public class MapsController : Controller
+	{
+	
+		[HttpGet]
+		public ActionResult Index()
+		{
+			var cities = new List<City>();
+			cities.Add(new City() { Title = "Paris", Lat = 48.855901, Lng = 2.349272 });
+			cities.Add(new City() { Title = "Berlin", Lat = 52.520413, Lng = 13.402794 });
+			cities.Add(new City() { Title = "Rome", Lat = 41.907074, Lng = 12.498474 });
+			cities.Add(new City() { Title = "Belgrade", Lat = 44.7866, Lng = 20.4489 });
+			return View(cities);
+		}
+
+
+		[HttpPost]
+		public JsonResult GetAnswer(string question)
+		{
+			int index = _rnd.Next(_db.Count);
+			var answer = _db[index];
+			return Json(answer);
+		}
+
+		private static Random _rnd = new Random();
+
+		private static List<string> _db = new List<string> { "Yes", "No", "Definitely, yes", "I don't know", "Looks like, yes" };
+	}
+
+}
---
+++ Models/City.cs
@@ -0,0 +1,11 @@
+﻿namespace VideoKlub.Models
+{
+    public class City
+    {
+        public string Title { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Lng { get; set; }
+    }
+}
